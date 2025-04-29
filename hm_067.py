569dd07550c1d805d69c2aecfee781e81df735e5
Amoer_Han <hanamoer@gmail.com>
2025-04-29 15:07:39 +1000

    There are some note and code about input function

--- hm_067.py
+++ hm_067.py
@@ -1,5 +1,7 @@
 password = input("Please enter your banking password: ")
 print(password)
+password
+print("This is a test to connection to git")
 print(password)
-print(password)
-print("This is a test to connection to   git")
+print(type(password))   #in the phchram there is no result in using type( ),
+                        # it's different with ipython3 running in terminal.
