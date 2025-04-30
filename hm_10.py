22328517f6bfca0e2fc44def401308d4946b20c2
Amoer_Han <hanamoer@gmail.com>
2025-05-01 00:09:38 +1000

    the name of varies

--- hm_10.py
+++ hm_10.py
@@ -3,3 +3,5 @@ print(keyword.kwlist)
 
 #the rule of the name for varies
 # if combine with two words, require two of the word is lower, connect together by using "_"
+
+#the other way is Camel Case which is the first word lower, the second word upper, for example :"firstWord","FirstWord"
