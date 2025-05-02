125adf051255acdd560903e92c642b00ed30b3f6
Amoer_Han <hanamoer@gmail.com>
2025-05-02 11:56:46 +1000

    the name of varies

--- hm_10.py
+++
@@ -1,7 +0,0 @@
-import keyword
-print(keyword.kwlist)
-
-#the rule of the name for varies
-# if combine with two words, require two of the word is lower, connect together by using "_"
-
-#the other way is Camel Case which is the first word lower, the second word upper, for example :"firstWord","FirstWord"
---
+++ hm_81.py
@@ -0,0 +1,30 @@
+"""
+import keyword
+print(keyword.kwlist)
+"""
+
+#the rule of the name for varies
+# if combine with two words, require two of the word is lower, connect together by using "_"
+
+#the other way is Camel Case which is the first word lower, the second word upper, for example:"firstWord","FirstWord"
+
+"""
+if rule, use 4 times space instead of the Tab
+
+"""
+age = int(input("age: "))
+if age >= 18:
+    Tab = "Approved to enter this place"
+elif age <= 18 or age >= 60 :
+    Tab = "Not approved to enter this place"
+print(Tab)
+
+"""
+== equal to 
+!= unequal to 
+> more than
+< less than
+>= more than or equal to
+<= less than or equal to 
+"""
+
