d9cd4bd0638add883fabcde55543a4198ea5c6e7
Amoer_Han <hanamoer@gmail.com>
2025-05-08 10:38:45 +1000

    9 * 9 Multiplication Table\

--- While loop/hm_125.py
+++ While loop/hm_125.py
@@ -14,11 +14,24 @@ while row_1 <= 5:
 '''
 row_1 = 1
 col_1 = 1
-while row_1 <= 5:
+while row_1 <= 9:
     col_1 = 1
-    print("*",end = "")
+    print(f"{col_1} * {col_1} = {col_1 * col_1}",end = " ")
     while col_1 < row_1:
-        print("*",end = "")
+        print(f"{col_1} * {row_1} = {col_1 * row_1}",end = " ")
+        col_1 += 1
+    print("")
+    row_1 += 1
+
+#\t, \n, can be using in different situation, and can using \ to Escape character
+print("This is a 9 * 9 \"Multiplication Table\"")
+row_1 = 1
+col_1 = 1
+while row_1 <= 9:
+    col_1 = 1
+    print(f"{col_1} * {col_1} = {col_1 * col_1}",end = "\t")
+    while col_1 < row_1:
+        print(f"{col_1} * {row_1} = {col_1 * row_1}",end = "\t")
         col_1 += 1
     print("")
     row_1 += 1
