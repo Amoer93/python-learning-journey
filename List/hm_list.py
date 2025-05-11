bcad9bd03701c04b5f9923094ce3100df6ff3d6f
Amoer_Han <hanamoer@gmail.com>
2025-05-11 23:37:17 +1000

    insert and append to modify data which are belong to list

---
+++ List/hm_list.py
@@ -0,0 +1,20 @@
+name_list = ["Jam","Porter","Peter"] #input data into list
+print(name_list) #show the list
+
+print(name_list[1]) #output the second data
+
+name_list.append("Jack")
+print(name_list) #show the list
+
+# the maximem
+#print(name_list[4])
+
+#find data's index
+# print(name_list.index("Jam"))
+# print(name_list.index("Jam1")) # if the data which is input does not belong to the list, will be error
+
+#change data
+name_list[1] = "Harry"
+print(name_list[1])
+name_list.insert(1,"Honey")
+print(name_list[1])
