8288ad300caa98677293776f3b91719ae5457702
Amoer_Han <hanamoer@gmail.com>
2025-05-14 23:12:09 +1000

    string count,len,index

--- hm_190.py
+++ hm_190.py
@@ -1,5 +1,7 @@
 #字符串定义，遇到需要增加特殊字符，可以用\+字符来实现
-str1 = "我的名字是\"大西瓜\""
+from itertools import count
+
+str1 = "我的名字是\"大的西瓜\""
 print(str1)
 key =str1[5]
 print(key)
@@ -7,3 +9,15 @@ print(key)
 # 用for循环把每个小的字符提取出来
 for char in str1:
     print(char)
+
+#len 函数
+print(len(str1))
+
+#count 数某一元素出现多少次
+print(str1.count("的"))
+
+#取得索引，index
+print(str1.index("的"))
+
+#可调用的功能
+str1.
